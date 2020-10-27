86207b2f46f96bcafb382b5da30cef3b458babc0
Fin Kingma <fin.kingma@praegus.nl>
2020-10-27 11:15:43 +0100

    cleaned up a bit more

--- src/test/java/io/componenttesting/teammorale/EventRouterApplicationTests.java
+++ src/test/java/io/componenttesting/teammorale/EventRouterApplicationTests.java
@@ -1,7 +1,7 @@
 package io.componenttesting.teammorale;
 
 import io.componenttesting.teammorale.config.ApplicationConfig;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
